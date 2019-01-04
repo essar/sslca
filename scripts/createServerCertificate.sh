8977cd4e2e651fdf4bf4595b34292ca2434751a6
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2019-01-04 10:22:37 +0000

    Added support for SubjectAlternateName

--- scripts/createServerCertificate.sh
+++ scripts/createServerCertificate.sh
@@ -8,6 +8,7 @@ if [ $# -lt 2 ]; then
   echo '  crtfile: file to create containing the signed certificate'
   echo '  lifetime_days: number of days to sign the certificate for (default 365 days)'
   echo '  subj: subject to use in the certificate'
+  echo '  sans: list of Subject Alternate Names to add to the certificate'
   exit 1
 fi
 
@@ -16,8 +17,9 @@ KEYFILE=$1
 CRTFILE=$2
 LIFE_DAYS=${3:-365} # Default to 1 year
 SUBJ="$4"
+SANS="$5"
 
 mydir=$(dirname "$0")
 
-$mydir/createSignedCertificate.sh $KEYFILE $CRTFILE v3_svr $LIFE_DAYS "$SUBJ"
+$mydir/createSignedCertificate.sh $KEYFILE $CRTFILE v3_svr $LIFE_DAYS "$SUBJ" "$SANS"
 
--- scripts/createSignedCertificate.sh
+++ scripts/createSignedCertificate.sh
@@ -3,12 +3,13 @@
 # Check args
 if [ $# -lt 3 ]; then
   echo 'Creates a certificate and signs with the current CA'
-  echo 'Usage: createSignedCertificate.sh <keyfile> <crtfile> <exts> [lifetime_days] [subj]'
+  echo 'Usage: createSignedCertificate.sh <keyfile> <crtfile> <exts> [lifetime_days] [subj] [sans]'
   echo '  keyfile: file containing the private key'
   echo '  crtfile: file to create containing the signed certificate'
   echo '  exts: the extensions to apply to the certificate'
   echo '  lifetime_days: number of days to sign the certificate for (default 365 days)'
   echo '  subj: subject to use in the certificate'
+  echo '  sans: list of Subject Alternate Names to add to the certificate'
   exit 1
 fi
 
@@ -18,6 +19,7 @@ CRTFILE=$2
 EXTS=$3
 LIFE_DAYS=${4:-365} # Default to 1 year
 SUBJ="$5"
+SANS="$6"
 
 if [ ! -f $CADIR/intermediate.key ] || [ ! -f $CADIR/intermediate.crt ]; then
   echo CA not found, are you in the CA directory?
@@ -39,7 +41,9 @@ echo  Creating certificate
 echo ================================
 
 # Create the CSR
-if [ ! -z "$SUBJ" ]; then
+if [ ! -z "$SANS" ]; then
+  openssl req -new -config <(cat ../openssl.cnf <(printf "\n[ SAN ]\nsubjectAltName=$SANS")) -out $CRTFILE.csr -key $KEYFILE -subj "$SUBJ" -reqexts SAN || exit 1
+elif [ ! -z "$SUBJ" ]; then
   openssl req -new -config ../openssl.cnf -out $CRTFILE.csr -key $KEYFILE -subj "$SUBJ" || exit 1
 else
   openssl req -new -config ../openssl.cnf -out $CRTFILE.csr -key $KEYFILE || exit 1
@@ -51,8 +55,8 @@ echo  Signing certificate
 echo ================================
 
 # Sign the certificate using the intermediate CA
-(openssl ca -config ../openssl.cnf -name int_CA -in $CRTFILE.csr -out $CRTFILE -cert $CADIR/intermediate.crt -keyfile $CADIR/intermediate.key -notext -days $LIFE_DAYS -extensions $EXTS && chmod 444 $CRTFILE) && chmod 444 $CRTFILE || exit 1
+(openssl ca -config ../openssl.cnf -name int_CA -in $CRTFILE.csr -out $CRTFILE -cert $CADIR/intermediate.crt -keyfile $CADIR/intermediate.key -extensions $EXTS -notext -days $LIFE_DAYS && chmod 444 $CRTFILE) && chmod 444 $CRTFILE || exit 1
 
 # Remove the CSR
-rm $CRTFILE.csr
+#rm $CRTFILE.csr
 
